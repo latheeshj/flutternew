03dc0d47f3117427ef575462024988ef2a380367
Gautam Latheesh <cheepilat@gmail.com>
2020-12-23 22:55:33 +1100

    second comm23

--- lib/main - Copy.dart
+++ lib/main - Copy.dart
@@ -109,17 +109,5 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-    Container(
-      color: Colors.black,
-      child: Transform(
-        alignment: Alignment.topRight,
-        transform: Matrix4.skewY(0.3)..rotateZ(-math.pi / 12.0),
-        child: Container(
-          padding: const EdgeInsets.all(8.0),
-          color: const Color(0xFFE8581C),
-          child: const Text('Apartment for rent!'),
-        ),
-      ),
-    )
   }
 }
